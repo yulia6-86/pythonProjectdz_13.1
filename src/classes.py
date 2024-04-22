af9b4b03b1761d8d63da797b213ab83d44f051ec
yulia6-86 <yulia6-86@mail.ru>
2024-04-22 12:40:49 +0300

    dz13.3

--- src/classes.py
+++ src/classes.py
@@ -30,6 +30,19 @@ class Category:
         return output
 
 
+    def __repr__(self):
+        return f"{self.__class__.__name__} ({self.name} {self.description} {self.__products})"
+
+
+    def __len__(self):
+        """Магический метод для применения функции len"""
+        return len (self.__products)
+
+    def __str__(self):
+        """ Вывод кол-ва продуктов в следующем виде: 'Название категории, количество продуктов: 200 шт.' """
+        return f"Название категории {self.name}, количество продуктов: {self.__len__()} шт."
+
+
 class Product:
     "Создаем класс продуктов с атрибутами имя,описание,цена,количество"
     name: str
@@ -46,7 +59,12 @@ class Product:
 
 
     def __repr__(self):
-        return f"'{self.name}', '{self.description}', {self.price}, {self.quantity}"
+        return f"{self.__class__.__name__} ({self.name} {self.description} {self.price} {self.quantity})"
+
+
+    def __str__(self):
+        """ Вывод списка продуктов в определенном виде"""
+        return f"{self.name}, {self.price} руб Остаток: {self.quantity} шт."
 
     @classmethod
     def create_product(cls,name,description,price,quantity):
@@ -79,6 +97,11 @@ class Product:
 
         else:
             self.price = value
+
+
+    def __add__(self, other):
+        """Магический метод add для возможности сложения экземпляров Класса"""
+        return self.quantity * self.price + other.quantity * other.price
 
 
 
--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ print (category_tv.name)
 print (Category.all_quantity_category)
 print (Category.all_quantity_unique_product)
 
-new_prod= Product.create_prodact("Miele", "Описание Miele",18000,88)
+new_prod= Product.create_product("Miele", "Описание Miele",18000,88)
 new_prod.get_price =50000
 
 print (new_prod)
@@ -19,3 +19,7 @@ category_tv.add_products(ex_3)
 
 new= category_tv.list_products
 print (new)
+
+print(category_tv)
+
+print (ex_1 + ex_2)
